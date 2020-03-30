207a118ff75c27a458575ce5f9b72a008b1d1ce7
duguyining <2580500887@qq.com>
2020-03-30 22:40:19 +0800

    js执行上下文

--- JS/stack01.js
+++ JS/stack01.js
@@ -15,6 +15,7 @@ var foo = function () {
 foo();
 
 /* ————————区分———————— */
+
 /* JavaScript 引擎并非一行一行地分析和执行程序，而是一段一段地分析执行。当执行一段代码的时候，会进行一个“准备工作”，比如第一个例子中的变量提升，和第二个例子中的函数提升。 */
 // 执行上下文(execution context)
 function foo() {
@@ -22,10 +23,11 @@ function foo() {
     console.log('foo1');
 
 }
-foo();  // foo2
+foo();  
 function foo() {
 
     console.log('foo2');
 
 }
-foo(); // foo2
+foo(); 
+// 两次结果都是foo2
