721fbc23680dc5f15b726248848f6fab740ff565
duguyining <2580500887@qq.com>
2020-03-30 22:33:09 +0800

    JavaScript深入之执行上下文栈

---
+++ JS/stack01.js
@@ -0,0 +1,34 @@
+var foo = function () {
+
+    console.log('foo1');
+
+}
+
+foo();  // foo1
+
+var foo = function () {
+
+    console.log('foo2');
+
+}
+
+foo();
+
+/* ————————区分———————— */
+/* JavaScript 引擎并非一行一行地分析和执行程序，而是一段一段地分析执行。当执行一段代码的时候，会进行一个“准备工作”，比如第一个例子中的变量提升，和第二个例子中的函数提升。 */
+// 执行上下文(execution context)
+function foo() {
+
+    console.log('foo1');
+
+}
+
+foo();  // foo2
+
+function foo() {
+
+    console.log('foo2');
+
+}
+
+foo(); // foo2
