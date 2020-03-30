15755f7b45db942f7dcfb757af4c2c381bd4be2a
duguyining <2580500887@qq.com>
2020-03-30 18:46:00 +0800

    JavaScript深入之从原型到原型链

---
+++ JS/prototype01.js
@@ -0,0 +1,23 @@
+function Person () {
+	
+}
+
+var person = new Person();
+console.log(person.__proto__ === Person.prototype); // true
+console.log(Person === Person.prototype.constructor); // true
+console.log(Object.getPrototypeOf(person) === Person.prototype) // true
+person.name='Jack';
+console.log(person.name);
+
+// prototype是函数才会有的属性
+Person.prototype.name='Kevin';
+var person1=new Person();
+var person2=new Person();
+console.log(person1.name);
+console.log(person2.name);
+
+
+// 原型的原型
+var obj = new Object();
+obj.name = 'Kevin'
+console.log(obj.name) // Kevin
